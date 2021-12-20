edac870dbfd638a86f2124a57be2c716da370b2b
neqelovek <neqelovek@gmail.com>
2021-12-20 09:02:46 +0300

    Прописал вывод кнопок

--- app/src/main/java/ru/gb/calculator/MainActivity.java
+++ app/src/main/java/ru/gb/calculator/MainActivity.java
@@ -1,21 +1,336 @@
 package ru.gb.calculator;
-//Start
-//1. С этого урока будем писать приложение «Калькулятор».
-// Выберите макет для работы с калькулятором.
-// Обоснуйте, почему будете использовать именно этот тип макета.
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final static String keyCounters = "Counters";
+
+    private Counters counters;
+    private TextView textCounters;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle instanceState) {
+        super.onSaveInstanceState(instanceState);
+        instanceState.putSerializable(keyCounters, counters);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle instanceState) {
+        super.onRestoreInstanceState(instanceState);
+        counters = (Counters) instanceState.getSerializable(keyCounters);
+        setTetCounters();
+    }
+
+    private void setTetCounters() {
+        setTextCounter(textCounters, counters.getValue3());
+        setTextCounter(textCounters, counters.getValue2());
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear_layout);
 
-
+        counters = new Counters();
+        initView();
 
     }
+
+    private void initView() {
+        textCounters = findViewById(R.id.field_for_numbers);
+        initButton1ClickListener();
+        initButton2ClickListener();
+        initButton3ClickListener();
+        initButton4ClickListener();
+        initButton5ClickListener();
+        initButton6ClickListener();
+        initButton7ClickListener();
+        initButton8ClickListener();
+        initButton9ClickListener();
+        initButton0ClickListener();
+        initButtonPlusClickListener();
+        initButtonMinusClickListener();
+        initButtonPointClickListener();
+        initButtonPercentClickListener();
+        initButtonEquallyClickListener();
+        initButtonCleanClickListener();
+        initButtonMultiplyClickListener();
+        initButtonToShareClickListener();
+        initButtonCleanAllClickListener();
+        initButtonMemoryReadClickListener();
+        initButtonMemoryMinusClickListener();
+        initButtonMemoryPlusClickListener();
+        initButtonMemoryCleanClickListener();
+        initButton0ClickListener();
+        initButton0ClickListener();
+    }
+
+    private void initButton1ClickListener() {
+        Button first_button = findViewById(R.id.first_button);
+        first_button.setOnClickListener(this);
+    }
+
+    private void initButton2ClickListener() {
+        Button two_button = findViewById(R.id.two_button);
+        two_button.setOnClickListener(this);
+    }
+
+    private void initButton3ClickListener() {
+        Button three_button = findViewById(R.id.three_button);
+        three_button.setOnClickListener(this);
+    }
+
+    private void initButtonMemoryCleanClickListener() {
+        Button memory_clean_button = findViewById(R.id.memory_clean_button);
+        memory_clean_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonMemoryPlusClickListener() {
+        Button memory_plus_button = findViewById(R.id.memory_plus_button);
+        memory_plus_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonMemoryMinusClickListener() {
+        Button memory_minus_button = findViewById(R.id.memory_minus_button);
+        memory_minus_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonMemoryReadClickListener() {
+        Button memory_read_button = findViewById(R.id.memory_read_button);
+        memory_read_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonCleanAllClickListener() {
+        Button clean_all_button = findViewById(R.id.clean_all_button);
+        clean_all_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonToShareClickListener() {
+        Button to_share_button = findViewById(R.id.to_share_button);
+        to_share_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonMultiplyClickListener() {
+        Button multiply_button = findViewById(R.id.multiply_button);
+        multiply_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonCleanClickListener() {
+        Button clean_button = findViewById(R.id.clean_button);
+        clean_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonEquallyClickListener() {
+        Button equally_button = findViewById(R.id.equally_button);
+        equally_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonPercentClickListener() {
+        Button percent_button = findViewById(R.id.percent_button);
+        percent_button.setOnClickListener(this);
+    }
+
+    private void initButtonPointClickListener() {
+        Button point_button = findViewById(R.id.point_button);
+        point_button.setOnClickListener(this);
+    }
+
+
+    private void initButtonPlusClickListener() {
+        Button plus_button = findViewById(R.id.plus_button);
+        plus_button.setOnClickListener(this);
+    }
+
+    private void initButtonMinusClickListener() {
+        Button minus_button = findViewById(R.id.minus_button);
+        minus_button.setOnClickListener(this);
+    }
+
+    private void initButton0ClickListener() {
+        Button zero_button = findViewById(R.id.zero_button);
+        zero_button.setOnClickListener(this);
+    }
+
+    private void initButton9ClickListener() {
+        Button nine_button = findViewById(R.id.nine_button);
+        nine_button.setOnClickListener(this);
+    }
+
+    private void initButton8ClickListener() {
+        Button eight_button = findViewById(R.id.eight_button);
+        eight_button.setOnClickListener(this);
+    }
+
+    private void initButton7ClickListener() {
+        Button seven_button = findViewById(R.id.seven_button);
+        seven_button.setOnClickListener(this);
+    }
+
+    private void initButton6ClickListener() {
+        Button six_button = findViewById(R.id.six_button);
+        six_button.setOnClickListener(this);
+    }
+
+    private void initButton5ClickListener() {
+        Button five_button = findViewById(R.id.five_button);
+        five_button.setOnClickListener(this);
+
+    }
+
+    private void initButton4ClickListener() {
+        Button four_button = findViewById(R.id.four_button);
+        four_button.setOnClickListener(this);
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.first_button:
+                setTextCounter(textCounters, counters.getValue1());
+
+                break;
+
+            case R.id.two_button:
+                setTextCounter(textCounters, counters.getValue2())
+                ;
+                break;
+            case R.id.three_button:
+                setTextCounter(textCounters, counters.getValue3());
+                break;
+            case R.id.four_button:
+
+                setTextCounter(textCounters, counters.getValue4());
+                break;
+            case R.id.five_button:
+                setTextCounter(textCounters, counters.getValue5());
+                break;
+            case R.id.six_button:
+                setTextCounter(textCounters, counters.getValue6());
+                break;
+            case R.id.seven_button:
+                setTextCounter(textCounters, counters.getValue7());
+                break;
+            case R.id.eight_button:
+                setTextCounter(textCounters, counters.getValue8());
+                break;
+            case R.id.nine_button:
+                setTextCounter(textCounters, counters.getValue9());
+                break;
+            case R.id.zero_button:
+                setTextCounter(textCounters, counters.getValue0());
+                break;
+
+        }
+
+    }
+
+    private void setTextCounter(TextView textCounters, int counter) {
+        textCounters.setText(String.format(Locale.getDefault(), "%d", counter));
+    }
 }
+
+
+//  case R.id.to_share_button:
+//          setTextCounter(textCounters, counters.getValueShare());
+//          break;
+//          case R.id.percent_button:
+//          setTextCounter(textCounters, counters.getValuePercent());
+//          break;
+//          case R.id.plus_button:
+//          setTextCounter(textCounters, counters.getValuePlus());
+//          break;
+//          case R.id.point_button:
+//          setTextCounter(textCounters, counters.getValuePoint());
+//          break;
+//          case R.id.minus_button:
+//          setTextCounter(textCounters, counters.getValueMinus());
+//          break;
+//          case R.id.multiply_button:
+//          setTextCounter(textCounters, counters.getValueMultiply());
+//          break;
+//          case R.id.equally_button:
+//          setTextCounter(textCounters, counters.getValueEqually());
+//          break;
+//          case R.id.memory_plus_button:
+//          setTextCounter(textCounters, Integer.parseInt(counters.getValueMemoryMinus()));
+//          break;
+//          case R.id.memory_clean_button:
+//          setTextCounter(textCounters, Integer.parseInt(counters.getValueMemoryClean()));
+//          break;
+//          case R.id.memory_minus_button:
+//          setTextCounter(textCounters, Integer.parseInt(counters.getValueMemoryMinus()));
+//          break;
+//          case R.id.memory_read_button:
+//          setTextCounter(textCounters, Integer.parseInt(counters.getValueMemoryRead()));
+//          break;
+//          case R.id.clean_all_button:
+//          setTextCounter(textCounters, Integer.parseInt(counters.getValueCleanAll()));
+//          break;
+//          case R.id.clean_button:
+//          setTextCounter(textCounters, counters.getValueClean());
+//          break;
+
+
+//     field_for_numbers = (TextView) findViewById(R.id.field_for_numbers);
+//        Button memory_clean_button = findViewById(R.id.memory_clean_button);
+//        Button memory_plus_button = findViewById(R.id.memory_plus_button);
+//        Button memory_minus_button = findViewById(R.id.memory_minus_button);
+//        Button memory_read_button = findViewById(R.id.memory_read_button);
+//        Button clean_all_button = findViewById(R.id.clean_all_button);
+//        Button to_share_button = findViewById(R.id.to_share_button);
+//        Button multiply_button = findViewById(R.id.multiply_button);
+//        Button clean_button = findViewById(R.id.clean_button);
+//        Button seven_button = findViewById(R.id.seven_button);
+//        Button eight_button = findViewById(R.id.eight_button);
+//        Button nine_button = findViewById(R.id.nine_button);
+//        Button minus_button = findViewById(R.id.minus_button);
+//        Button four_button = findViewById(R.id.four_button);
+//        Button five_button = findViewById(R.id.five_button);
+//        Button six_button = findViewById(R.id.six_button);
+//        Button plus_button = findViewById(R.id.plus_button);
+//        first_button = (Button) findViewById(R.id.first_button);
+//        two_button = (Button) findViewById(R.id.two_button);
+//        three_button = (Button) findViewById(R.id.three_button);
+//        Button point_button = findViewById(R.id.point_button);
+//        Button percent_button = findViewById(R.id.percent_button);
+//        Button zero_button = findViewById(R.id.zero_button);
+//        Button equally_button = findViewById(R.id.equally_button);
+//
+//        first_button.setOnClickListener(this);
+//        two_button.setOnClickListener(this);
+//        three_button.setOnClickListener(this);
+
+
+//        first_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                field_for_numbers.setText(String.format(Locale.getDefault(), "%d", value.valueButton1));
+//
+//            }
+//        });
